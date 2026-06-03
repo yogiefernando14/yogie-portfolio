5d86f5e10e8a374150fcd640625bc33b788ae6a2
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-04 01:22:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -330,3 +330,96 @@ document.body.style.opacity =
 
 }
 );
+
+/* =========================
+   ACTIVE NAVIGATION
+========================= */
+
+const sections =
+document.querySelectorAll("section");
+
+const navLinks =
+document.querySelectorAll(".nav-links a");
+
+window.addEventListener(
+"scroll",
+()=>{
+
+let current = "";
+
+sections.forEach((section)=>{
+
+const sectionTop =
+section.offsetTop - 150;
+
+if(window.scrollY >= sectionTop){
+
+current =
+section.getAttribute("id");
+
+}
+
+});
+
+navLinks.forEach((link)=>{
+
+link.classList.remove("active");
+
+if(
+link.getAttribute("href")
+=== "#" + current
+){
+
+link.classList.add("active");
+
+}
+
+});
+
+/* =========================
+   ACTIVE NAVIGATION
+========================= */
+
+const sections =
+document.querySelectorAll("section");
+
+const navLinks =
+document.querySelectorAll(".nav-links a");
+
+window.addEventListener(
+"scroll",
+()=>{
+
+let current = "";
+
+sections.forEach((section)=>{
+
+const sectionTop =
+section.offsetTop - 150;
+
+if(window.scrollY >= sectionTop){
+
+current =
+section.getAttribute("id");
+
+}
+
+});
+
+navLinks.forEach((link)=>{
+
+link.classList.remove("active");
+
+if(
+link.getAttribute("href")
+=== "#" + current
+){
+
+link.classList.add("active");
+
+}
+
+});
+
+}
+);
