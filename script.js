a081182df2e1eec9877b54038b96d040f23964a8
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-03 23:58:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const revealElements = document.querySelectorAll(
+".section,.card,.hero-content"
+);
 
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+
+const trigger =
+window.innerHeight * 0.85;
+
+revealElements.forEach((el)=>{
+
+const top =
+el.getBoundingClientRect().top;
+
+if(top < trigger){
+el.classList.add("active");
+}
+
+});
+
+}
+
+revealElements.forEach((el)=>{
+el.classList.add("reveal");
+});
+
+reveal();
